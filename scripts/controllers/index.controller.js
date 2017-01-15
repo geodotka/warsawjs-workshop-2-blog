a6780a5341a4d2ca7ac1b0a13fae58c5ffa3d72d
geodotka <dorota.kowalik@21s.pl>
2017-01-15 14:31:47 +0100

    Add simple post page

--- scripts/controllers/index.controller.js
+++ scripts/controllers/index.controller.js
@@ -6,6 +6,35 @@ let $page = document.getElementById('page');
 
 routie('', controller);
 
+
 export function controller() {
-    PostListComponent.render({posts:[]}, $page)
+    PostService.list().then(
+        posts=>{
+            PostListComponent.render({posts:posts}, $page)
+        }
+//        err=>console.log(err) to ma sens jeśli zdefiniujemy reject w funkcji list
+    )
 }
+
+
+/// promise explanation
+
+//let UserService = {login: function(){}};
+//export function controller() {
+//    let promise = UserService.login()
+//
+//      .then(user=>{
+//        return PostService.list(user)
+//      }, err=>{
+////         return UserService.loginAlternative()
+//        return Promise.reject('nie da rady');
+//      })
+//
+//     .then(posts=>{
+//        posts.push({nowy:'record'})
+//        return UserService.update(posts);
+//     })
+//    .then(postsSavedMessage=>{
+//        PostListComponent.render({posts:posts}, $page)
+//     })
+//}
